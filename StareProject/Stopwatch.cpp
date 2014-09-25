cc9b6dafdf5a28b747508a7d9d007390d8e98931
Brian Davis <ringzero.d9@gmail.com>
2014-09-25 13:06:28 -0400

    Included sql lite studio so I stop loosing it.

---
+++ StareProject/Stopwatch.cpp
@@ -0,0 +1,43 @@
+#include "Stopwatch.h"
+
+
+Stopwatch::Stopwatch()
+{
+	swState = stopwatchState::clear;
+}
+
+
+Stopwatch::~Stopwatch()
+{
+}
+
+
+void Stopwatch::start()
+{
+	if (swState != stopwatchState::running)
+	{
+		startTime = std::chrono::steady_clock::now();
+		swState = stopwatchState::running;
+	}
+}
+
+
+
+void Stopwatch::end()
+{
+	if (swState == stopwatchState::running)
+	{
+		endTime = std::chrono::steady_clock::now();
+		swState = stopwatchState::stopped;
+	}
+	else if (swState == stopwatchState::clear)
+		throw "Stopwatch has not started yet.";
+	else
+		throw "Stopwatch has already stopped.";
+}
+
+int Stopwatch::getTimeInMicroseconds()
+{
+	auto elapsed = std::chrono::duration_cast<std::chrono::microseconds> (endTime - startTime);
+	return elapsed.count();
+}
