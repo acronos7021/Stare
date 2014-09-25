14159a430ea38245a7bb8fd6d7be3e6c94347806
acronos7021 <ringzero.d9@gmail.com>
2014-09-24 22:03:27 -0400

    Merged Sams code into the project

--- StareProject/Tokenizer.cpp
+++ StareProject/Tokenizer.cpp
@@ -1,22 +1,151 @@
 #include "Tokenizer.h"
 #include <sstream>
+using namespace std;
 
+//takes care of everything aside from and empty lines in the document ATM
 
 Tokenizer::Tokenizer(string Document)
 {
-	nextChar = 0;
+	filename = Document;
+	proceed = false;
+	punctuation[0] = '.';
+	punctuation[1] = '!';
+	punctuation[2] = '?';
+};
+
+Tokenizer:: ~Tokenizer()
+{
+	//nada
 }
 
-
-Tokenizer::~Tokenizer()
+bool Tokenizer::checkIgnore(char c)
 {
+	if ((c >= 33 && c <= 47 || c >= 58 && c <= 64 || c >= 91 && c <= 96 || c >= 123 && c <= 126 || c >= 128 || c >= 10 && c <= 13) && (c != 39 && c != 96))
+	{
+		return true;
+	}
+	return false;
 }
 
-vector<string> Tokenizer::getNextSentence()
+bool Tokenizer::checkPunctuation(char c)
 {
-	vector<string> words;
+	for (unsigned i = 0; i < sizeof(punctuation); i++)
+	{
+		if (c == (char)punctuation[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
-	return words;
+void Tokenizer::tokenizeDoc()
+{
+	ifstream fname(filename);
+	string currline;
+	vector <string> curr;
+	vector <string> temp;
+	if (!fname.is_open())
+	{
+		printf("\nAn error has occured and the file does not exist!!!\n");
+		return;
+	}
+
+	while (!fname.eof())
+	{
+		if (fname.peek() == '\n' || fname.peek() == '\r')
+		{
+			curr.push_back("\n"); // use /n instead of \n for illustration puposes only
+		}
+		fname >> currline;
+		curr.push_back(currline);
+		for (unsigned i = 0; i < currline.size(); i++)
+		{
+			if (checkIgnore((char)currline[i]) || checkPunctuation((char)currline[i]))
+			{
+				curr.pop_back();
+				temp.clear();
+				temp = getWord(currline);
+				for (int i = 0; i < temp.size(); i++)
+				{
+					curr.push_back(temp[i]);
+				}
+				if (checkPunctuation((char)currline[i]))
+				{
+					proceed = true;
+				}
+				break;
+			}
+		}
+		if (proceed)
+		{
+			sentences.push_back(curr);
+			curr.clear();
+			proceed = false;
+		}
+	}
+} //end of method
+
+vector<string> Tokenizer::getWord(string word)
+{
+	vector<string> v;
+	vector<char>letters;
+	string temp;
+
+	for (int i = 0; i < word.size(); i++)
+	{
+		letters.push_back(word[i]);
+	}
+	for (int i = 0; i < letters.size(); i++)
+	{
+		if (!checkIgnore(letters[i]))
+		{
+			temp.push_back(letters[i]);
+		}
+		else
+		{
+			if (temp != "")
+			{
+				v.push_back(temp);
+			}
+
+			temp.clear();
+			temp = letters[i];
+
+			if (temp != "")
+			{
+				v.push_back(temp);
+			}
+			temp.clear();
+		}
+	}
+	return v;
+}
+
+void Tokenizer::print_BAV()
+{
+	for (int i = 0; i < sentences.size(); i++)
+	{
+		for (int j = 0; j < sentences[i].size(); j++)
+		{
+			cout << sentences[i][j] << " ";
+		}
+	}
+}
+
+vector <string> Tokenizer::getNextSentence()
+{
+	vector<string> temp;
+	if (sentences.size() < 1)
+	{
+		return temp;
+	}
+	else
+	{
+		temp = sentences[0];
+		sentences.erase(sentences.begin());
+		return temp;
+	}
 }
 
 vector<string> Tokenizer::getCommands(string cmdStr)
@@ -82,7 +211,7 @@ vector<string> Tokenizer::getCommands(string cmdStr)
 			}
 		}
 	}
-	
+
 	// The last word can end with the end of the string.  
 	// It would not be detected then, so add it here.
 	if (inWord)
@@ -100,3 +229,120 @@ bool Tokenizer::isAlphaNumeric(char c)
 	else
 		return false;
 }
+
+/*int main()
+{
+string testfile = "text.txt";
+Sentence test(testfile);
+
+test.tokenizeDoc();
+test.print_BAV();
+
+cout << endl;
+system("pause");
+}*/
+
+
+
+//#include "Tokenizer.h"
+//#include <sstream>
+//
+//
+//Tokenizer::Tokenizer(string Document)
+//{
+//	nextChar = 0;
+//}
+//
+//
+//Tokenizer::~Tokenizer()
+//{
+//}
+//
+//vector<string> Tokenizer::getNextSentence()
+//{
+//	vector<string> words;
+//
+//	return words;
+//}
+//
+//vector<string> Tokenizer::getCommands(string cmdStr)
+//{
+//	vector<string> words;
+//	stringstream ss;
+//
+//	// I was too lazy to implement a full state machine.  However...
+//	// Uses 3 states to determine the action at a character position.  They are:
+//	// inWord==false, inQuotes==false :  this state means that any non Alphanums are dumped until a proper alphanum is encountered
+//	// inWord==true, inQuotes==false  :  This state meant that any alphanums are added to the current word until a non-alphanum is encountered
+//	// inWord==true, inQuotes==true  :  Anything, whether alphanum or not, is added to the current word until another '"' is encountered.
+//
+//	bool inWord = false;   // if inWord, all alphanumeric 
+//	bool inQuotes = false; // if inQuotes is true, all characters are recorded into the current ss until another quote is encountered.
+//
+//	for (unsigned int i = 0; i < cmdStr.size(); i++)
+//	{
+//		if (!inWord)
+//		{
+//			if (isAlphaNumeric(cmdStr[i]))
+//			{
+//				// start word
+//				ss << cmdStr[i];
+//				inWord = true;
+//			}
+//			else if (cmdStr[i] == '"')
+//			{
+//				// start quotes
+//				inWord = true;
+//				inQuotes = true;
+//			}
+//		}
+//		else
+//		{
+//			// inWord is true
+//			if (inQuotes)
+//			{
+//				if (cmdStr[i] != '"')
+//					ss << cmdStr[i];
+//				else
+//				{
+//					// the quotes have ended
+//					words.push_back(ss.str());
+//					ss.str("");  // empty ss
+//					inWord = false;
+//					inQuotes = false;
+//
+//				}
+//			}
+//			else
+//			{
+//				if (isAlphaNumeric(cmdStr[i]))
+//					ss << cmdStr[i];
+//				else
+//				{
+//					// the word has ended
+//					words.push_back(ss.str());
+//					ss.str("");  // empty ss
+//					inWord = false;
+//				}
+//
+//			}
+//		}
+//	}
+//	
+//	// The last word can end with the end of the string.  
+//	// It would not be detected then, so add it here.
+//	if (inWord)
+//		words.push_back(ss.str());
+//
+//	return words;
+//}
+//
+//bool Tokenizer::isAlphaNumeric(char c)
+//{
+//	if (((c >= 'a') && (c <= 'z')) ||
+//		((c >= 'A') && (c <= 'Z')) ||
+//		((c >= '0') && (c <= '9')))
+//		return true;
+//	else
+//		return false;
+//}
--- StareProject/Tokenizer.h
+++ StareProject/Tokenizer.h
@@ -1,22 +1,62 @@
 #pragma once
-
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
 #include "DataStructs.h"
 
 class Tokenizer
 {
 public:
-	Tokenizer(string Document);
+	bool proceed;
+	std::vector <std::vector<std::string>> sentences;
+	char punctuation[3];
+
+	Tokenizer(std::string Document);
 	~Tokenizer();
 
-	// returns a single sentence from the Document
-	// returns an empty vector if there is no next sentence.
-	vector<string> getNextSentence();
+	bool checkIgnore(char c);
+	bool checkPunctuation(char c);
+
+	std::vector<std::string> getWord(std::string word);
+	std::string checkTabRet(std::string word);
+
+	//tokenizes the entire document and fills up the sentences vector
+	void tokenizeDoc();
+	void print_BAV();
+
+	//returns a single sentence from the document
+	//returns an empty vector if there is no sentence left
+	std::vector<std::string> getNextSentence();
 
 	static vector<string> getCommands(string cmdStr);
 	static bool isAlphaNumeric(char c);
 
 private:
-	int nextChar;
-
+	std::string filename;
 };
+
+
+//#pragma once
+//
+//#include "DataStructs.h"
+//
+//class Tokenizer
+//{
+//public:
+//	Tokenizer(string Document);
+//	~Tokenizer();
+//
+//	// returns a single sentence from the Document
+//	// returns an empty vector if there is no next sentence.
+//	vector<string> getNextSentence();
+//
+//	static vector<string> getCommands(string cmdStr);
+//	static bool isAlphaNumeric(char c);
+//
+//private:
+//	int nextChar;
+//
+//};
 
