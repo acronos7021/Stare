ae34f81b32981a09381ea7a4fb9187f07bbdda5a
Blake Peterman <peterman.blake@gmail.com>
2014-09-28 12:35:37 -0400

    Added support for an empty vector
    Now if the vector contains no matches we'll tell the user "Your document
    was not plagiarized!"

--- StareProject/HTMLgenerator.cpp
+++ StareProject/HTMLgenerator.cpp
@@ -23,35 +23,42 @@ HTMLgenerator::HTMLgenerator(string DestinationFilename, int documentID, vector<
 	infile >> header;
 	output << header;
 	infile.close();
-	//Output how much your document was plagarised
-	// ********I don't know how to call this, any ideas? insert between the bold tags(There is no input for this either)
-	output << "<p>Your document was plagiarized <b>" << "</b></p>";
 
-	for (std::vector<sentenceRanking>::size_type i = 0; i != rankingList.size(); i++)
+	if (rankingList.size!=0){
+		//Output how much your document was plagarised
+		// ********I don't know how to call this, any ideas? insert between the bold tags(There is no input for this either)
+		output << "<p>Your document was plagiarized <b>" << "</b></p>";
+
+		for (std::vector<sentenceRanking>::size_type i = 0; i != rankingList.size(); i++)
+		{
+			//Generate the left column first (That's the document that the user gave us)
+			output << "<div id='doc-wrapper'><div id='left-column'>";
+			output << db.getSentence(rankingList[i].sentenceID - 2) << "  ";
+			output << db.getSentence(rankingList[i].sentenceID - 1) << "  ";
+			output << "<div id='popup' style='background-color: yellow;'>";
+			output << db.getSentence(rankingList[i].sentenceID) << "  ";
+			//add spans for probability
+			output << "<span>This section was plagiarized: <b>" << rankingList[i].certainty << "</b></span></div>";
+			output << db.getSentence(rankingList[i].sentenceID + 1) << "  ";
+			output << db.getSentence(rankingList[i].sentenceID + 2) << "  ";
+			output << "</div'>"; //end left-column
+
+
+			output << "<div id='right-column'>"; //begin right column
+			output << db.getSentence(rankingList[i].foundSentenceID - 2) << "  ";
+			output << db.getSentence(rankingList[i].foundSentenceID - 1) << "  ";
+			output << "<div id='popup' style='background-color: yellow;'>";
+			output << db.getSentence(rankingList[i].foundSentenceID) << "  ";
+			output << "<span>This section was plagiarized: <b>" << rankingList[i].certainty << "</b></span></div>";
+			output << db.getSentence(rankingList[i].foundSentenceID + 1) << "  ";
+			output << db.getSentence(rankingList[i].foundSentenceID + 2) << "  ";
+
+			output << "</div>"; //end the doc-wrapper
+		}
+	}
+	else
 	{
-		//Generate the left column first (That's the document that the user gave us)
-		output << "<div id='doc-wrapper'><div id='left-column'>";
-		output << db.getSentence(rankingList[i].sentenceID - 2) << "  ";
-		output << db.getSentence(rankingList[i].sentenceID - 1) << "  ";
-		output << "<div id='popup' style='background-color: yellow;'>";
-		output << db.getSentence(rankingList[i].sentenceID) << "  ";
-		//add spans for probability
-		output << "<span>This section was plagiarized: <b>" << rankingList[i].certainty << "</b></span></div>";
-		output << db.getSentence(rankingList[i].sentenceID + 1) << "  ";
-		output << db.getSentence(rankingList[i].sentenceID + 2) << "  ";
-		output << "</div'>"; //end left-column
-
-
-		output << "<div id='right-column'>"; //begin right column
-		output << db.getSentence(rankingList[i].foundSentenceID - 2) << "  ";
-		output << db.getSentence(rankingList[i].foundSentenceID - 1) << "  ";
-		output << "<div id='popup' style='background-color: yellow;'>";
-		output << db.getSentence(rankingList[i].foundSentenceID) << "  ";
-		output << "<span>This section was plagiarized: <b>" << rankingList[i].certainty << "</b></span></div>";
-		output << db.getSentence(rankingList[i].foundSentenceID + 1) << "  ";
-		output << db.getSentence(rankingList[i].foundSentenceID + 2) << "  ";
-
-		output << "</div>"; //end the doc-wrapper
+		output << "<p>Your document was not plagiarized!</p>";
 	}
 
 	//Basically I just want to take the contents of the footer.txt file and put it in the output stream here
