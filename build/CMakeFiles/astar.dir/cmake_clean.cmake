e678f5f988d08a08a560098f07c90c6d2b16e084
Brandon <shadowxp2006@gmail.com>
2014-10-28 08:35:13 -0400

    Adds the build directory and removes .gitignore
    .gitignore was responsible for causing the build directory to not be
    included

---
+++ build/CMakeFiles/astar.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/astar.dir/sqlite3.c.o"
+  "CMakeFiles/astar.dir/main.cpp.o"
+  "CMakeFiles/astar.dir/CMDparser.cpp.o"
+  "CMakeFiles/astar.dir/Console.cpp.o"
+  "CMakeFiles/astar.dir/Database.cpp.o"
+  "CMakeFiles/astar.dir/HMMengine.cpp.o"
+  "CMakeFiles/astar.dir/HTMLgenerator.cpp.o"
+  "CMakeFiles/astar.dir/Stopwatch.cpp.o"
+  "CMakeFiles/astar.dir/Tokenizer.cpp.o"
+  "astar.pdb"
+  "astar"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/astar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
