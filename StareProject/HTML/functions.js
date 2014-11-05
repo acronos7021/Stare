c109a4c692ca3c6bf71050ed2311a576e1fa6b56
Blake Peterman <peterman.blake@gmail.com>
2014-11-04 23:52:52 -0500

    Finished compare JSON display

--- StareProject/HTML/functions.js
+++ StareProject/HTML/functions.js
@@ -1,29 +1,50 @@
 function compareDecode(json) {
     var response = JSON.parse(json);
-    //alert(response.clientID);
-    //alert(response.ranking[0].origSnip[0]);
-    var div = document.createElement('div');
-    div.setAttribute("id", "section");
-    var inside="";
-    //title section
-    inside += '<div id="doc-wrapper"><div id="left-column"><h3>'+response.ranking[0].documentTitle+'</h3></div>';
-    inside += '<div id="right-column"><h3>'+response.ranking[0].documentTitle+'</h3></div></div>';
-    
-    //content section
-    inside += '<div id="doc-wrapper"><div id="left-column"><div id=doc-box>'+ response.ranking[0].origSnip[0] +'</div></div>';
-    inside += '<div id="right-column"><div id=doc-box>'+ response.ranking[0].dataBaseSnip[0] +'</div></div></div>';
-    div.innerHTML=inside;
-    document.getElementById('plag').appendChild(div);
-    
+
+    //add the overall certainty
+    document.getElementById('certainty').innerHTML = response.overallCertainty;
+
+    //Start body of plagarized document
+	for (i=0; i < response.ranking.length; ++i){
+	    var div = document.createElement('div');
+	    div.setAttribute("id", "section");
+	    var inside = "";
+	    //title section
+	    inside += '<div id="doc-wrapper"><div id="left-column"><h3>' +
+	        response.ranking[i].documentTitle + '</h3></div>';
+	    inside += '<div id="right-column"><h3>' + response.ranking[i]
+	        .documentTitle + '</h3></div></div>';
+	
+	    //content section
+	    inside +=
+	        '<div id="doc-wrapper"><div id="left-column"><div id=doc-box>' +
+	        response.ranking[i].origSnip[0] +
+	        '  <div id="popup" style="background-color: yellow;">' +
+	        response.ranking[i].origSnip[1] +
+	        '<span>This section was plagiarized: <b>' + response.ranking[
+	            i].certainty + '%</b></span></div>  ' + response.ranking[
+	            i].origSnip[2] + '</div></div>';
+	    inside += '<div id="right-column"><div id=doc-box>' +
+	        response.ranking[i].dataBaseSnip[0] +
+	        '  <div id="popup" style="background-color: yellow;">' +
+	        response.ranking[i].dataBaseSnip[1] +
+	        '<span>This section was plagiarized: <b>' + response.ranking[
+	            i].certainty + '%</b></span></div>  ' + response.ranking[
+	            i].dataBaseSnip[2] + '</div></div></div>';
+	    div.innerHTML = inside;
+	    document.getElementById('plag').appendChild(div);
+	}
 }
 
 
 
 
-compareDecode('{"clientID":"ID","command":"compare","status":"#of characters process","documentText":"the document text here","ranking":[{"origSnip":["sentence1","sentence2","sentence3"],"dataBaseSnip":["sentence1","sentence2","sentence3"],"certainty":"someValue","documentTitle":"someTitle","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"},{"origSnip":["sentence1","sentence2","sentence3"],"dataBaseSnip":["sentence1","sentence2","sentence3"],"certainty":"someValue","documentTitle":"someTitle","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"}]}');
+compareDecode(
+    '{"clientID":"ID","command":"compare","status":"#of characters process","documentText":"the document text here","overallCertainty":"50","ranking":[{"origSnip":["Aenean suscipit a metus eu aliquet.","Vestibulum lacus lorem, viverra sit amet tincidunt a, pharetra vitae mi.","Aliquam egestas rutrum tincidunt."],"dataBaseSnip":["Lorem ipsum dolor sit amet, consectetur adipiscing elit.","In molestie congue libero ut feugiat.","Etiam convallis arcu sit amet elit ullamcorper, rhoncus sodales lorem consequat."],"certainty":"someValue","documentTitle":"someTitle","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"},{"origSnip":["sentence1","sentence2","sentence3"],"dataBaseSnip":["sentence1","sentence2","sentence3"],"certainty":"someValue","documentTitle":"2nd Document","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"}]}'
+);
 
 
 
-function sendCompare(clientID, text){
+function sendCompare(clientID, text) {
     //do something to send it off here
 }
