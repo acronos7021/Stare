85b8115778a2c036cc86cb27140e70fb2cc781e8
Blake Peterman <peterman.blake@gmail.com>
2014-11-11 12:54:48 -0500

    Compare Upload's Functionality is rock solid.  Now for appearance.

--- StareProject/HTML/functions.js
+++ StareProject/HTML/functions.js
@@ -1,9 +1,10 @@
 function compareDecode(json) {
     var response = JSON.parse(json);
     
-    //TODO add most of HTML for output page.  removed from template due to progress bar.
-    //TODO replace output HTML with what would be needed for update page instead.
-    
+    var outputHTML = '<html><head><title>STARE</title><link rel="stylesheet" type="text/css" href="style.css" /></head><div id="wrapper"><h1>STARE</h1><p>This is STARE. A plagiarism detector.</p><p>Your document was plagiarized: <b><span id="certainty"></span>%</b></p><div id="plagiarism"><!-- This is where javascript inserts compare --></div></div><script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js"></script><script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script><script src="functions.js"></script></html>';
+    document.open();
+    document.write(outputHTML);
+    document.close();
     //add the overall certainty
     document.getElementById('certainty').innerHTML = response.overallCertainty;
     //Start body of plagarized document
@@ -13,7 +14,7 @@ function compareDecode(json) {
 	    var inside = "";
 	    //title area
 	    inside += '<div id="doc-wrapper"><div id="left-column"><h3>' +
-	        userDocTitle + '</h3></div>';
+	        window.userDocTitle + '</h3></div>';
 	    inside += '<div id="right-column"><h3>' + response.ranking[i]
 	        .documentTitle + '</h3></div></div>';
 	    //content area
@@ -44,10 +45,9 @@ function sendCompare(clientID, text) {
 		"command": "compare",
 		"documentText": text
 	}
-	
     //do something to send it off here
     sendMessage(compare);
-    window.location.href = 'output.html';
+    //window.location.href = 'output.html';
 }
 
 function checkCompare(id){
@@ -76,11 +76,14 @@ function sendMessage(string){
 function callback(response){
     var json = JSON.parse(response);
     if(json.command=="compare"){
-        //compareDecode(JSON.stringify(json));
-        window.location.href = 'output.html'; 
+        if(json.hasOwnProperty("ranking")){
+            compareDecode(JSON.stringify(json));
+            return;
+        }
+        //window.location.href = 'output.html'; 
     }
     else if(json.command=="checkCompare"){
-        if(json.hasOwnProperty("documentText")){
+        if(json.hasOwnProperty("ranking")){
             compareDecode(JSON.stringify(json));
             return;
         }
@@ -136,6 +139,16 @@ function generateID(){
 	return d.getTime();
 }
 
+// getElementById
+function $id(id) {
+	return document.getElementById(id);
+}
+//output info
+function Output(msg) {
+	var m = $id("messages");
+	m.innerHTML = msg;
+}
+
 function readSingleFile(evt) {
     //Retrieve the first (and only!) File from the FileList object
     var f = evt.target.files[0];
@@ -153,17 +166,7 @@ function readSingleFile(evt) {
         alert("Failed to load file");
     }
 }
-//sendCompare("TheID", "The document text will go here");
-//This is for testing
-var userDocTitle = "My Title"; // this will be set using a cookie in the future.
-//compareDecode(
-//    '{"clientID":"ID","command":"compare","status":"#of characters process","documentText":"the document text here","overallCertainty":"50","ranking":[{"origSnip":["Aenean suscipit a metus eu aliquet.","Vestibulum lacus lorem, viverra sit amet tincidunt a, pharetra vitae mi.","Aliquam egestas rutrum tincidunt."],"dataBaseSnip":["Lorem ipsum dolor sit amet, consectetur adipiscing elit.","In molestie congue libero ut feugiat.","Etiam convallis arcu sit amet elit ullamcorper, rhoncus sodales lorem consequat."],"certainty":"10","documentTitle":"someTitle","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"},{"origSnip":["sentence1","sentence2","sentence3"],"dataBaseSnip":["sentence1","sentence2","sentence3"],"certainty":"100","documentTitle":"2nd Document","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"}]}'
-//);
 
-//test generate id
-//alert("Your ID is:\n"+generateID());
-var compare = {"command":"compare","overallCertainty":"100","ranking":[{"origSnip":["sentence1","sentence2","sentence3"],"dataBaseSnip":["sentence1","sentence2","sentence3"],"certainty":"80","documentTitle":"someTitle","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"},{"origSnip":["sentence1","sentence2","sentence3"],"dataBaseSnip":["sentence1","sentence2","sentence3"],"certainty":"90","documentTitle":"someTitle","foundDocumentID":"SomeDocID","foundSentenceID":"someSentID"}]}
-//sendMessage(compare);
 
 
 
